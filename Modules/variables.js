4c06acaf9eec2fd4f6090cdd14642e8420b4a2ba
Vivek Ranjan <36644502+vivek007ranjan@users.noreply.github.com>
2023-09-19 10:49:50 +0000

    variables modules added

---
+++ Modules/variables.js
@@ -0,0 +1,10 @@
+"use strict";
+//Declare variables
+let name = "Vivek Ranjan"
+let Id = 12345
+const company ="Vinimoy.com"
+
+//print on console
+console.table([name,Id,company])
+
+//alert("Vivek Ranjan") node js has different way to run alert
