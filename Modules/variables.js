a264090b3255d5d38781ee85431e8329386cf414
Vivek Ranjan <36644502+vivek007ranjan@users.noreply.github.com>
2023-09-19 11:06:10 +0000

    new types

--- Modules/variables.js
+++ Modules/variables.js
@@ -3,8 +3,13 @@
 let name = "Vivek Ranjan"
 let Id = 12345
 const company ="Vinimoy.com"
+let IsEmployee = true
+
+//null
+//undefined
+//symbol - uniqueness
 
 //print on console
-console.table([name,Id,company])
+console.table([name,Id,company,IsEmployee])
 
 //alert("Vivek Ranjan") node js has different way to run alert
