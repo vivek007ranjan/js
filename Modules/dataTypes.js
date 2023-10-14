4a87b1cb230cf91b990fae0acc8d275ca0ef20fd
Vivek Ranjan <36644502+vivek007ranjan@users.noreply.github.com>
2023-10-14 06:28:33 +0000

    DatatypesSummary

---
+++ Modules/dataTypes.js
@@ -0,0 +1,26 @@
+//primitive : String, Number, boolean, null, undefined Symbol
+let name = 'Vivek Ranjan';
+const id = 3214;
+let EptSal;
+const currntsal = 1000000n;
+const comp = Symbol(123);
+console.log(typeof(currntsal));
+
+// Reference / Non primitive type : Arrays , Object, Functions
+//Arrays:
+const heros = ["Shaktiman","Nagraj","Doga", "Dhruv", 100, 2.5, true, EptSal, comp,currntsal];
+console.table(heros);
+let index = [];
+const indi = [];
+console.table(indi);
+
+//object:
+
+
+// functions:
+
+
+
+
+
+
